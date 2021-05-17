6836c8af71655366be4c64c8d4535cf90a0270e9
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-17 23:09:58 +0200

    Added base model

---
+++ models/base.model.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jinzhu/gorm"
+)
+
+type Base struct {
+	ID        string `sql:"type:uuid;primary_key;default:uuid_generate_v4()" validate:"omitempty,uuid,required"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	DeletedAt string `sql:"index" json:"deleted_at"`
+}
+
+func (b *Base) BeforeCreate(scope *gorm.Scope) error {
+	id, err := uuid.NewRandom()
+	if err != nil {
+		errWrapped := fmt.Sprintf("Cannot generate new Id : %s", err.Error())
+		return errors.New(errWrapped)
+	}
+	return scope.SetColumn("id", id)
+}
+
+func (b *Base) GetID() string {
+	return b.ID
+}
+
+func (b *Base) SetID(newId string) error {
+	if len(newId) == 0 {
+		return errors.New("new id is empty")
+	}
+	b.ID = newId
+	return nil
+}
+
+func (b *Base) SetCreatedAt(t time.Time) {
+	b.CreatedAt = t.String()
+}
+
+func (b *Base) SetUpdatedAt(t time.Time) {
+	b.UpdatedAt = t.String()
+}
+
+func (b *Base) SetDeletedAt(t time.Time) {
+	b.DeletedAt = t.String()
+}
