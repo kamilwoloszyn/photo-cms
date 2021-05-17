07859e55a18ecb5fec064a8a37a013d9f8331b66
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-17 23:09:15 +0200

    Added server cnf

---
+++ configs/server.config.go
@@ -0,0 +1,46 @@
+package configs
+
+import (
+	"errors"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	Env    string `default:"dev" envconfig:"ENV"`
+	Host   string `default:"0.0.0.0:3000" envconfig:"HOST"`
+	DB     DbConfig
+	Engine *gin.Engine
+	Routes *gin.RouterGroup
+}
+
+func New() (*Server, error) {
+
+	dbConfig, err := LoadDbConfig()
+	if err != nil {
+		return nil, err
+	}
+	if dbConfig == nil {
+		return nil, errors.New("cannot load database config")
+	}
+	server := Server{
+		Env:    os.Getenv("ENV"),
+		Host:   os.Getenv("HOST"),
+		DB:     *dbConfig,
+		Engine: gin.Default(),
+		Routes: nil,
+	}
+	return &server, nil
+}
+
+func (s *Server) Listen() error {
+	return s.Engine.Run()
+}
+
+func (s *Server) ApplyRoutes() {
+	s.Routes = s.Engine.Group("api")
+	{
+		s.Routes.GET("/ping")
+	}
+}
