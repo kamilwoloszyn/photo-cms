6dca51810095fabf4ee14b077a3d22e20f00c6ce
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-19 18:32:48 +0200

    Added Error

--- models/repository.go
+++ models/repository.go
@@ -7,12 +7,13 @@ import (
 )
 
 var (
-	handler *gorm.DB
+	handler         *gorm.DB
+	HandlerNotFound = errors.New("db is not connected.")
 )
 
 func SetHandler(newHandler *gorm.DB) error {
 	if newHandler == nil {
-		return errors.New("Handler is empty (db is not connected)")
+		return errors.New("handler is empty (db is not connected)")
 	}
 	handler = newHandler
 	return nil
