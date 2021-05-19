abee7ac4ef93d83360dd6e5aeaeb5e56b979027e
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-19 18:34:19 +0200

    Added customer model

---
+++ models/customer.model.go
@@ -0,0 +1,41 @@
+package models
+
+type Customer struct {
+	Base
+	City         string
+	Address      string
+	FirstName    string
+	LastName     string
+	PostalCode   string
+	CompanyName  string
+	PhoneNumber  string
+	EmailAddress string
+	Employed     bool
+	NIP          string
+	Regon        string
+}
+
+func (c *Customer) Create() error {
+	if handler == nil {
+		return HandlerNotFound
+	}
+
+	return handler.Create(c).Error
+}
+func (c *Customer) Delete() error {
+	return handler.Delete(c).Error
+}
+
+func (c *Customer) SetId(id string) {
+	if len(id) > 0 {
+		c.ID = id
+	}
+}
+
+func (c *Customer) FetchById() error {
+	if handler == nil {
+		return HandlerNotFound
+	}
+	return handler.Find(c).Error
+
+}
