16b4683a470e23f418e9c5546b532c91388f6d29
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-19 18:41:48 +0200

    Updated Product Option

--- models/product_option.model.go
+++ models/product_option.model.go
@@ -3,4 +3,25 @@ package models
 type ProductOption struct {
 	Base
 	OptionValues []OptionValue
+	Products     []Product
+}
+
+func (p *ProductOption) Create() error {
+	if handler == nil {
+		return HandlerNotFound
+	}
+	return handler.Create(p).Error
+}
+func (p *ProductOption) Delete() error {
+	if handler == nil {
+		return HandlerNotFound
+	}
+	return handler.Delete(p).Error
+}
+func (p *ProductOption) FetchById() error {
+	if handler == nil {
+		return HandlerNotFound
+	}
+	return handler.First(p).Error
+
 }
