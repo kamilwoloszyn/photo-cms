c0d1ec8cab9c2667607a0b841f90d7371cf5b9ae
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-19 18:34:11 +0200

    Recreated migrations

--- migrations/migrate.go
+++ migrations/migrate.go
@@ -6,10 +6,17 @@ import (
 )
 
 func Migrate(db *gorm.DB) {
-	db.AutoMigrate(&models.PaperType{})
-	db.AutoMigrate(&models.Format{})
-	db.AutoMigrate(&models.Photo{})
-	db.AutoMigrate(&models.Print{})
+	db.AutoMigrate(&models.Option{})
+	db.AutoMigrate(&models.OptionValue{})
+	db.AutoMigrate(&models.ProductOption{})
+	db.AutoMigrate(&models.Customer{})
+	db.AutoMigrate(&models.Image{})
+	db.AutoMigrate(&models.DeliveryMethod{})
+	db.AutoMigrate(&models.Category{})
+	db.AutoMigrate(&models.PaymentMethod{})
+	db.AutoMigrate(&models.Payment{})
+	db.AutoMigrate(&models.Delivery{})
+	db.AutoMigrate(&models.Product{})
 	db.AutoMigrate(&models.Order{})
 
 }
