29290bbc79b6fac2e78667181eb0595b390b4e5f
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-20 20:15:15 +0200

    Added tests for option model

---
+++ models/option.model_test.go
@@ -0,0 +1,12 @@
+package models_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Option model test", func() {
+	Context("Basic crud testing", func() {
+
+	})
+})
