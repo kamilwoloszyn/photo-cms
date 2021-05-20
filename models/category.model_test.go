c650e2f607a3db572c22f52a480403579a281190
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-20 20:20:26 +0200

    Added file for testing Category model

---
+++ models/category.model_test.go
@@ -0,0 +1,13 @@
+package models_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Category Model", func() {
+
+	Context("Crud operations", func() {
+
+	})
+})
