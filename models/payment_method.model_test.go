f204dbd38b72f2428eaf6ac7fe16a9d9f51b7864
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-20 20:13:43 +0200

    Added testing file for PaymentMethod model

---
+++ models/payment_method.model_test.go
@@ -0,0 +1,12 @@
+package models_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Payment Method model", func() {
+	Context("Basic crud testing", func() {
+
+	})
+})
