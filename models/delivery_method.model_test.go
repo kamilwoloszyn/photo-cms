931ac51b7f1929d0b9ccc0b7e4d5ee79928b6c9e
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-20 20:19:12 +0200

    Added file for delivery model testing

---
+++ models/delivery_method.model_test.go
@@ -0,0 +1,12 @@
+package models_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("DeliveryMethod model", func() {
+	Context("Basic crud operations", func() {
+
+	})
+})
