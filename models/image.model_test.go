25909e23e6462d61d5695e90b68d798bd9239c1d
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-20 20:16:28 +0200

    Added file tests for image model

---
+++ models/image.model_test.go
@@ -0,0 +1,12 @@
+package models_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Image Model", func() {
+	Context("Basic CRUD test", func() {
+
+	})
+})
