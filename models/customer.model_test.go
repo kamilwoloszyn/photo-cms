27ae30d591521ff8c5ac5a5a98a4c629b259ae1f
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-20 20:19:49 +0200

    Added test file for customer model testing

---
+++ models/customer.model_test.go
@@ -0,0 +1,12 @@
+package models_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Customer model", func() {
+	Context("Crud Basic", func() {
+
+	})
+})
