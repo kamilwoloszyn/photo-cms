72c8a5e6f153dfa27edb41893c94d615960f9c72
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-20 20:12:26 +0200

    Added tests for ProductOption model

---
+++ models/product_option.model_test.go
@@ -0,0 +1,12 @@
+package models_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Product Option test", func() {
+	Context("Basic crud testing", func() {
+
+	})
+})
