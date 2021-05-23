b0550bc68ff7cf63c89eb5b53d68f0f203ec4fae
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-23 21:18:40 +0200

    Added create test method for category model

--- models/category.model_test.go
+++ models/category.model_test.go
@@ -1,13 +1,27 @@
 package models_test
 
 import (
+	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Category Model", func() {
-
+	var category models.Category
+	BeforeEach(func() {
+		category = CreateCategory()
+	})
+	AfterEach(func() {
+		category.Delete()
+	})
 	Context("Crud operations", func() {
-
+		It("Should be stored into db", func() {
+			var obtainedCategory models.Category
+			err := obtainedCategory.SetID(category.GetID())
+			Expect(err).To(BeNil())
+			err = obtainedCategory.FetchById()
+			Expect(err).To(BeNil())
+			Expect(obtainedCategory.CategoryName).To(Equal(category.CategoryName))
+		})
 	})
 })
