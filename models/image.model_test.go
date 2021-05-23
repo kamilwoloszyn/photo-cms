2c2a87e7b15750ea181a8c62ffae0c4ea14221f7
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-23 21:19:13 +0200

    Added create test method for image model

--- models/image.model_test.go
+++ models/image.model_test.go
@@ -1,12 +1,29 @@
 package models_test
 
 import (
+	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Image Model", func() {
-	Context("Basic CRUD test", func() {
+	var image models.Image
 
+	BeforeEach(func() {
+		image = CreateImage()
+	})
+
+	AfterEach(func() {
+		image.Delete()
+	})
+
+	Context("Basic CRUD test", func() {
+		It("Should be stored into db", func() {
+			var obtainedImage models.Image
+			obtainedImage.SetID(image.GetID())
+			err := obtainedImage.FetchById()
+			Expect(err).To(BeNil())
+			Expect(obtainedImage.FullPath).To(Equal(image.FullPath))
+		})
 	})
 })
