51535b9837c1aba70fc9827483e063936f5af683
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-23 21:20:16 +0200

    Added create test method for payment method model

--- models/payment.model_test.go
+++ models/payment.model_test.go
@@ -1,12 +1,34 @@
 package models_test
 
 import (
+	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Payment model", func() {
-	Context("Basic crud testing", func() {
+	var (
+		paymentMethod models.PaymentMethod
+		payment       models.Payment
+	)
 
+	BeforeEach(func() {
+		paymentMethod = CreatePaymentMethod()
+		payment = CreatePayment(&paymentMethod)
+	})
+
+	AfterEach(func() {
+		payment.Delete()
+		paymentMethod.Delete()
+	})
+	Context("Basic crud testing", func() {
+		It("Should contain payment in db", func() {
+			var obtainedPayment models.Payment
+			err := obtainedPayment.SetID(payment.GetID())
+			Expect(err).To(BeNil())
+			err = obtainedPayment.FetchByID()
+			Expect(err).To(BeNil())
+			Expect(obtainedPayment.PaymentAmount).To(Equal(payment.PaymentAmount))
+		})
 	})
 })
--- models/payment_method.model_test.go
+++ models/payment_method.model_test.go
@@ -1,12 +1,29 @@
 package models_test
 
 import (
+	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Payment Method model", func() {
-	Context("Basic crud testing", func() {
+	var paymentMethod models.PaymentMethod
 
+	BeforeEach(func() {
+		paymentMethod = CreatePaymentMethod()
+	})
+
+	AfterEach(func() {
+		paymentMethod.Delete()
+	})
+	Context("Basic crud testing", func() {
+		It("Should be into db", func() {
+			var obtainedPaymentMethod models.PaymentMethod
+			err := obtainedPaymentMethod.SetID(paymentMethod.GetID())
+			Expect(err).To(BeNil())
+			err = obtainedPaymentMethod.FetchByID()
+			Expect(err).To(BeNil())
+			Expect(obtainedPaymentMethod.KeyMd5).To(Equal(paymentMethod.KeyMd5))
+		})
 	})
 })
