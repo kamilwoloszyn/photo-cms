1c3e0f8eae0ab2ad433f18cc9e0e483220598c34
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-23 21:19:20 +0200

    Added create test method for option model

--- models/option.model_test.go
+++ models/option.model_test.go
@@ -1,12 +1,28 @@
 package models_test
 
 import (
+	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Option model test", func() {
-	Context("Basic crud testing", func() {
 
+	var option models.Option
+
+	BeforeEach(func() {
+		option = CreateOption()
+	})
+	AfterEach(func() {
+		option.Delete()
+	})
+
+	Context("Basic crud testing", func() {
+		var obtainedOption models.Option
+		err := obtainedOption.SetID(option.GetID())
+		Expect(err).To(BeNil())
+		err = obtainedOption.FetchById()
+		Expect(err).To(BeNil())
+		Expect(obtainedOption.Name).To(Equal(option.Name))
 	})
 })
