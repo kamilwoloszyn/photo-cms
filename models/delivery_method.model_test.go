28bf59433d54936541746eb42bc738d93906c3fa
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-23 21:19:07 +0200

    Added create test method for delivery model

--- models/delivery_method.model_test.go
+++ models/delivery_method.model_test.go
@@ -1,12 +1,27 @@
 package models_test
 
 import (
+	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("DeliveryMethod model", func() {
-	Context("Basic crud operations", func() {
+	var deliveryMethod models.DeliveryMethod
+	BeforeEach(func() {
+		deliveryMethod = CreateDeliveryMethod()
+	})
 
+	AfterEach(func() {
+		deliveryMethod.Delete()
+	})
+	Context("Basic crud operations", func() {
+		It("Should be stored into db", func() {
+			var obtainedMethod models.DeliveryMethod
+			obtainedMethod.SetID(deliveryMethod.GetID())
+			err := obtainedMethod.FetchById()
+			Expect(err).To(BeNil())
+			Expect(obtainedMethod.Name).To(Equal(deliveryMethod.Name))
+		})
 	})
 })
