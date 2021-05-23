976ba9eb123f12f7f6216afbd316c5c80239fe56
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-23 21:18:55 +0200

    Added create test method for customer model

--- models/customer.model_test.go
+++ models/customer.model_test.go
@@ -1,12 +1,29 @@
 package models_test
 
 import (
+	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Customer model", func() {
-	Context("Crud Basic", func() {
+	var customer models.Customer
 
+	BeforeEach(func() {
+		customer = CreateCustomer()
+	})
+
+	AfterEach(func() {
+		customer.Delete()
+	})
+	Context("Crud Basic", func() {
+		It("Should be stored into db", func() {
+			var obtainedCustomer models.Customer
+			err := obtainedCustomer.SetID(customer.GetID())
+			Expect(err).To(BeNil())
+			err = obtainedCustomer.FetchById()
+			Expect(err).To(BeNil())
+			Expect(obtainedCustomer.City).To(Equal(customer.City))
+		})
 	})
 })
