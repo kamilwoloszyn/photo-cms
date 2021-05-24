020f465dfc85dcc266e69cd137a2148b0dc576c6
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-24 22:54:20 +0200

    Added tests for product model

--- models/product.model_test.go
+++ models/product.model_test.go
@@ -1,12 +1,58 @@
 package models_test
 
 import (
+	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Product model", func() {
-	Context("Basic crud testing", func() {
+	var category models.Category
+	var image models.Image
+	var option models.Option
+	var optionValue models.OptionValue
+	var productOption models.ProductOption
+	var customer models.Customer
+	var product models.Product
+
+	BeforeEach(func() {
+		category = CreateCategory()
+		image = CreateImage()
+		optionValue = CreateOptionValue()
+		option = CreateOption(&[]models.OptionValue{
+			optionValue,
+		})
+		customer = CreateCustomer()
+		product = CreateProductWithoutOrder(&category, &image, &customer)
+		productOption = CreateProductOption(&product, &optionValue)
+	})
+
+	AfterEach(func() {
+		productOption.Delete()
+		product.Delete()
+		customer.Delete()
+		optionValue.Delete()
+		category.Delete()
+		image.Delete()
+		option.Delete()
+	})
+
+	Describe("Basic crud testing", func() {
+		Context("Product without order", func() {
+			It("Should exist in db", func() {
+				var productObtained models.Product
+				err := productObtained.SetID(product.GetID())
+				Expect(err).To(BeNil())
+				err = productObtained.FetchByID()
+				Expect(err).To(BeNil())
+				Expect(productObtained.ProductName).To(Equal(product.ProductName))
+			})
+		})
+
+		Context("Product with order", func() {
+
+		})
 
 	})
+
 })
