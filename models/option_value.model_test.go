ec1ce2c3954f6e6d465fca222c05986ce6669c5a
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-24 22:48:36 +0200

    Added crud testing for OptionValue model

--- models/option_value.model_test.go
+++ models/option_value.model_test.go
@@ -1,12 +1,40 @@
 package models_test
 
 import (
+	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("OptionValue model", func() {
-	Context("Basic crud testing", func() {
+	var option models.Option
+	var optionValue models.OptionValue
+
+	BeforeEach(func() {
+		optionValue = CreateOptionValue()
+		option = CreateOption(&[]models.OptionValue{
+			optionValue,
+		})
+
+	})
+
+	AfterEach(func() {
+		option.Delete()
+		optionValue.Delete()
+	})
+
+	Describe("Basic crud testing", func() {
+		It("Should be in db", func() {
+			var obtainedOptionValue models.OptionValue
+			err := obtainedOptionValue.SetID(option.GetID())
+			Expect(err).To(BeNil())
+			err = obtainedOptionValue.FetchById()
+			Expect(err).To(BeNil())
+			Expect(obtainedOptionValue.Value).To(Equal(optionValue.Value))
+		})
+	})
+
+	Describe("Relationship test", func() {
 
 	})
 })
