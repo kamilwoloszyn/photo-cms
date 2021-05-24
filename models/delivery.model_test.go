66d6c929c85d9855bb19e97ec53faeb42fe8ce22
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-24 22:47:59 +0200

    Added crud operations test for delivery model

---
+++ models/delivery.model_test.go
@@ -0,0 +1,33 @@
+package models_test
+
+import (
+	"github.com/kamilwoloszyn/photo-cms/models"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Devliery model", func() {
+	var deliveryMethod models.DeliveryMethod
+	var delivery models.Delivery
+	BeforeEach(func() {
+		deliveryMethod = CreateDeliveryMethod()
+		delivery = CreateDelivery(&deliveryMethod)
+	})
+
+	AfterEach(func() {
+		delivery.Delete()
+		deliveryMethod.Delete()
+	})
+
+	Describe("Basic crud operations", func() {
+		It("Should be in db", func() {
+			obtainedDelivery := models.Delivery{}
+			err := obtainedDelivery.SetID(deliveryMethod.GetID())
+			Expect(err).To(BeNil())
+			err = obtainedDelivery.FetchById()
+			Expect(err).To(BeNil())
+			Expect(obtainedDelivery.DestinationCity).To(Equal(delivery.DestinationCity))
+		})
+	})
+
+})
