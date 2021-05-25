6a20c0f5d87cf103624b246f049161cbda4a13bd
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-25 22:12:13 +0200

    Fixed migrations

--- migrations/migrate_test.go
+++ migrations/migrate_test.go
@@ -2,6 +2,8 @@ package migrations_test
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/kamilwoloszyn/photo-cms/migrations"
+	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -10,15 +12,17 @@ var _ = Describe("Db Automigrate", func() {
 	var db *gorm.DB
 
 	BeforeEach(func() {
-
+		err := models.Connect()
+		Expect(err).To(BeNil())
+		db = models.GetHandler()
 	})
 
 	AfterEach(func() {
 		db.Close()
 	})
-	Context("Migration into db", func() {
+	Describe("Migration into db", func() {
 		It("Should migrate into db", func() {
-			Migrate()
+			migrations.Migrate(db)
 		})
 	})
 
