f458c6b7e5aa08d81404d1d65894f0d3ff95f7a3
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-25 16:28:17 +0200

    Changed err handler name for Delivery model

--- models/delivery.model.go
+++ models/delivery.model.go
@@ -13,21 +13,24 @@ type Delivery struct {
 
 func (d *Delivery) FetchById() error {
 	if handler == nil {
-		return HandlerNotFound
+		return ErrHandlerNotFound
 	}
 	return handler.First(d).Error
 }
 
 func (d *Delivery) Delete() error {
 	if handler == nil {
-		return HandlerNotFound
+		return ErrHandlerNotFound
 	}
 	return handler.Delete(d).Error
 }
 
 func (d *Delivery) Create() error {
 	if handler == nil {
-		return HandlerNotFound
+		return ErrHandlerNotFound
+	}
+	if len(d.ID) == 0 {
+		return ErrIdEmpty
 	}
 	return handler.Create(d).Error
 }
