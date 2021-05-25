787a700fa578977c89a389b3a37b7c12a863ed78
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-05-25 16:26:18 +0200

    Changed err handler name for category model

--- models/category.model.go
+++ models/category.model.go
@@ -7,21 +7,24 @@ type Category struct {
 
 func (c *Category) FetchById() error {
 	if handler == nil {
-		return HandlerNotFound
+		return ErrHandlerNotFound
 	}
 	return handler.First(c).Error
 }
 
 func (c *Category) Create() error {
 	if handler == nil {
-		return HandlerNotFound
+		return ErrHandlerNotFound
 	}
 	return handler.Create(c).Error
 }
 
 func (c *Category) Delete() error {
 	if handler == nil {
-		return HandlerNotFound
+		return ErrHandlerNotFound
+	}
+	if len(c.ID) == 0 {
+		return ErrIdEmpty
 	}
 	return handler.Delete(c).Error
 }
