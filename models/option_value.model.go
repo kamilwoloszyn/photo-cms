936b99b7075abddfb117df066d19f47c7509f030
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-06-01 16:58:00 +0200

    Fixed struct relationship, added relationship handlers

--- models/option_value.model.go
+++ models/option_value.model.go
@@ -1,12 +1,16 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+)
 
 type OptionValue struct {
 	Base
-	Value      string
-	ExtraPrice float32
-	OptionId   uuid.UUID
+	Value         string
+	ExtraPrice    float32
+	OptionId      uuid.UUID
+	ProductOption []ProductOption `gorm:"foreginKey:OptionValueId"`
 }
 
 func (o *OptionValue) FetchById() error {
@@ -28,15 +32,45 @@ func (o *OptionValue) Delete() error {
 	if handler == nil {
 		return ErrHandlerNotFound
 	}
-	if len(o.ID) == 0 {
+	if o.IsEmptyId() {
 		return ErrIdEmpty
 	}
 	return handler.Delete(o).Error
 }
 
-func (o *OptionValue) UpdateAll() error {
+func (o *OptionValue) UpdateInstance() error {
 	if handler == nil {
 		return ErrHandlerNotFound
 	}
 	return handler.Save(o).Error
+}
+
+func (o *OptionValue) AssignTo(po *ProductOption) error {
+	if handler == nil {
+		return ErrHandlerNotFound
+	}
+	if o.IsEmptyId() || po.IsEmptyId() {
+		return ErrIdEmpty
+	}
+	po.OptionValueId = o.GetID()
+	if err := po.UpdateInstance(); err != nil {
+		errWrapped := errors.Wrap(err, "Updating instance Product Option")
+		return errWrapped
+	}
+	return nil
+}
+
+func (o *OptionValue) GetProductOptions() error {
+	if handler == nil {
+		return ErrHandlerNotFound
+	}
+	if o.IsEmptyId() {
+		return ErrIdEmpty
+	}
+	tx := handler.Model(o).Select("options_values.id,product_options.id,product_options.product_id,product_options.option_value_id").Joins("left join product_options on product_options.option_value_id=options_values.id").Where("options_values.id=?", o.GetID()).Find(&o.ProductOption)
+	if tx.Error != nil {
+		errWrapped := errors.Wrap(tx.Error, "Join OptionValue with ProductOption")
+		return errWrapped
+	}
+	return nil
 }
