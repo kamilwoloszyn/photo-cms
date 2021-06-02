e404f9745a7c8dc2bbd40789f3641183256f168d
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-06-02 19:05:50 +0200

    checking empty id, implemented relationship function

--- models/product.model.go
+++ models/product.model.go
@@ -2,7 +2,7 @@ package models
 
 import (
 	"github.com/google/uuid"
-	"github.com/kamilwoloszyn/photo-cms/pkg/database"
+	"github.com/kamilwoloszyn/photo-cms/pkg/checkers"
 	_ "github.com/kamilwoloszyn/photo-cms/pkg/database"
 	"github.com/pkg/errors"
 )
@@ -23,11 +23,17 @@ func (p *Product) Create() error {
 	if handler == nil {
 		return ErrHandlerNotFound
 	}
+	if p.IsEmptyId() {
+		return ErrIdEmpty
+	}
 	return handler.Create(p).Error
 }
 func (p *Product) Delete() error {
 	if handler == nil {
 		return ErrHandlerNotFound
+	}
+	if p.IsEmptyId() {
+		return ErrIdEmpty
 	}
 	return handler.Delete(p).Error
 }
@@ -50,7 +56,7 @@ func (p *Product) AssignTo(po *ProductOption) error {
 	if handler == nil {
 		return ErrHandlerNotFound
 	}
-	if po.IsEmptyId() {
+	if p.IsEmptyId() {
 		return ErrIdEmpty
 	}
 	po.ProductId = p.GetID()
@@ -64,7 +70,7 @@ func (p *Product) GetCustomerDetails(c *Customer) error {
 	if handler == nil {
 		return ErrHandlerNotFound
 	}
-	if database.IsCorrectId(p.CustomerId.String()) {
+	if customerId := checkers.UuidGeneric(p.CustomerId); customerId.IsEmpty() {
 		return ErrIdEmpty
 	}
 	c.SetID(p.CustomerId)
@@ -79,18 +85,56 @@ func (p *Product) GetOrderDetails(o *Order) error {
 		return ErrHandlerNotFound
 	}
 
+	if orderId := checkers.UuidGeneric(p.OrderId); orderId.IsEmpty() {
+		return ErrIdEmpty
+	}
+	o.ID = p.OrderId
+	if err := o.FetchById(); err != nil {
+		errWrapped := errors.Wrap(err, "Fetching order details")
+		return errWrapped
+	}
 	return nil
 }
 func (p *Product) GetCategoryDetails(c *Category) error {
 	if handler == nil {
 		return ErrHandlerNotFound
 	}
+	if categoryId := checkers.UuidGeneric(p.CategoryId); categoryId.IsEmpty() {
+		return ErrIdEmpty
+	}
+	c.SetID(p.CategoryId)
+	if err := c.FetchById(); err != nil {
+		errWrapped := errors.Wrap(err, "Fetching category")
+		return errWrapped
+	}
 	return nil
 }
 
-func (p *Product) GetImageDetails(c *Category) error {
+func (p *Product) GetImageDetails(i *Image) error {
 	if handler == nil {
 		return ErrHandlerNotFound
+	}
+	if imageId := checkers.UuidGeneric(p.ImageId); imageId.IsEmpty() {
+		return ErrIdEmpty
+	}
+	i.SetID(p.ImageId)
+	if err := i.FetchById(); err != nil {
+		errWrapped := errors.Wrap(err, "Fetching Image")
+		return errWrapped
+	}
+	return nil
+}
+
+func (p *Product) GetProductOptions() error {
+	if handler == nil {
+		return ErrHandlerNotFound
+	}
+	if p.IsEmptyId() {
+		return ErrIdEmpty
+	}
+	tx := handler.Model(p).Select("products.id,product_options.id,product_options.created_at,product_options.updated_at,product_options.deleted_at,product_options.option_value_id,product_options.product_id").Joins("left join product_options on product_options.id=products.id").Where("products.id=?", p.GetID()).Find(&p.ProductOption)
+	if tx.Error != nil {
+		return errors.Wrap(tx.Error, "Join products with product_options")
 	}
 	return nil
 }
