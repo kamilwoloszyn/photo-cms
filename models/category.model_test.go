25dac0f2bf9a922b5c0d80c3cebaca4572c003d5
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-06-05 19:15:08 +0200

    crud & relationship tests

--- models/category.model_test.go
+++ models/category.model_test.go
@@ -4,24 +4,79 @@ import (
 	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"gorm.io/gorm"
 )
 
 var _ = Describe("Category Model", func() {
-	var category models.Category
+	var (
+		obtainedCategory models.Category
+		category         models.Category
+		image            models.Image
+		customers        models.Customer
+	)
 	BeforeEach(func() {
 		category = CreateCategory()
 	})
-	AfterEach(func() {
-		category.Delete()
-	})
 	Describe("Crud operations", func() {
-		It("Should be stored into db", func() {
-			var obtainedCategory models.Category
-			err := obtainedCategory.SetID(category.GetID())
-			Expect(err).To(BeNil())
-			err = obtainedCategory.FetchById()
-			Expect(err).To(BeNil())
-			Expect(obtainedCategory.CategoryName).To(Equal(category.CategoryName))
+
+		Context("Create or update", func() {
+			AfterEach(func() {
+				category.Delete()
+			})
+			It("Should create a new category", func() {
+				err := obtainedCategory.SetID(category.GetID())
+				Expect(err).ShouldNot(HaveOccurred())
+				err = obtainedCategory.FetchById()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(obtainedCategory.CategoryName).To(Equal(category.CategoryName))
+			})
+
+			It("Should update an existing category", func() {
+				category.CategoryName = "Changed"
+				err := category.UpdateInstance()
+				Expect(err).ShouldNot(HaveOccurred())
+				obtainedCategory.SetID(category.GetID())
+				err = obtainedCategory.FetchById()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(obtainedCategory.CategoryName).To(Equal("Changed"))
+			})
 		})
+
+		Context("Delete", func() {
+			It("Should delete an existing category", func() {
+				var testCategory models.Category
+				testCategory.SetID(category.GetID())
+				err := category.Delete()
+				Expect(err).ShouldNot(HaveOccurred())
+				err = testCategory.FetchById()
+				Expect(err).To(Equal(gorm.ErrRecordNotFound))
+				Expect(testCategory.CategoryName).To(Equal(""))
+			})
+		})
+
+	})
+	Describe("Relationship test", func() {
+		var product models.Product
+		Context("Category with product", func() {
+			BeforeEach(func() {
+				image = CreateImage()
+				customers = CreateCustomer()
+				product = CreateProductWithoutOrder(category, image, customers)
+			})
+			AfterEach(func() {
+				product.Delete()
+				image.Delete()
+				customers.Delete()
+				category.Delete()
+			})
+
+			It("Should have a product", func() {
+				err := category.FetchProducts()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(len(category.Product)).To(Equal(1))
+				Expect(product.CategoryId).To(Equal(category.GetID()))
+			})
+		})
+
 	})
 })
