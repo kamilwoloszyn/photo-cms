3857751730f1c727b8b29dd70d02d68857887411
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-06-05 19:13:59 +0200

    Added crud & relationship tests

--- models/delivery_method.model_test.go
+++ models/delivery_method.model_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"gorm.io/gorm"
 )
 
 var _ = Describe("DeliveryMethod model", func() {
@@ -16,12 +17,78 @@ var _ = Describe("DeliveryMethod model", func() {
 		deliveryMethod.Delete()
 	})
 	Describe("Basic crud operations", func() {
-		It("Should be stored into db", func() {
-			var obtainedMethod models.DeliveryMethod
-			obtainedMethod.SetID(deliveryMethod.GetID())
-			err := obtainedMethod.FetchById()
-			Expect(err).To(BeNil())
-			Expect(obtainedMethod.Name).To(Equal(deliveryMethod.Name))
+		Context("Create or Update operations", func() {
+			var obtainedDeliveryMethod models.DeliveryMethod
+
+			It("Should be stored into db", func() {
+				err := obtainedDeliveryMethod.SetID(deliveryMethod.GetID())
+				Expect(err).ShouldNot(HaveOccurred())
+				err = obtainedDeliveryMethod.FetchById()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(obtainedDeliveryMethod.Name).To(Equal(deliveryMethod.Name))
+			})
+
+			It("Should update an existing delivery method", func() {
+				err := obtainedDeliveryMethod.SetID(deliveryMethod.GetID())
+				Expect(err).ShouldNot(HaveOccurred())
+				deliveryMethod.FixedPrice = 40
+				err = deliveryMethod.UpdateInstance()
+				Expect(err).ShouldNot(HaveOccurred())
+				err = obtainedDeliveryMethod.FetchById()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(obtainedDeliveryMethod.FixedPrice).To(Equal(deliveryMethod.FixedPrice))
+			})
+			Context("Delete operations", func() {
+				It("Should delete a delivery method from db", func() {
+					err := obtainedDeliveryMethod.SetID(deliveryMethod.GetID())
+					Expect(err).ShouldNot(HaveOccurred())
+					err = deliveryMethod.Delete()
+					Expect(err).ShouldNot(HaveOccurred())
+					err = obtainedDeliveryMethod.FetchById()
+					Expect(err).To(Equal(gorm.ErrRecordNotFound))
+				})
+			})
 		})
 	})
+	Describe("Relationship tests", func() {
+		var (
+			delivery        models.Delivery
+			anotherDelivery models.Delivery
+		)
+		BeforeEach(func() {
+			delivery = CreateDelivery(deliveryMethod)
+		})
+		AfterEach(func() {
+			delivery.Delete()
+		})
+		Context("One delivery", func() {
+			It("Should get one delivery", func() {
+				err := deliveryMethod.GetDeliveries()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(deliveryMethod.Delivery[0].DestinationAddress).To(Equal(delivery.DestinationAddress))
+			})
+		})
+		Context("More deliveries", func() {
+			BeforeEach(func() {
+				anotherDelivery = models.Delivery{
+					ShippedVia:               "Jakub",
+					TrackingCode:             "123793002742342",
+					DestinationPostalCode:    "37-620",
+					DestinationCountryRegion: "Podkarpackie",
+					DestinationAddress:       "Truskawkowa 1",
+					DestinationCity:          "Horyniec-Zdroj",
+				}
+				CreateCustomDelivery(deliveryMethod, &anotherDelivery)
+			})
+			AfterEach(func() {
+				anotherDelivery.Delete()
+			})
+			It("Should get two deliveries", func() {
+				err := deliveryMethod.GetDeliveries()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(len(deliveryMethod.Delivery)).To(Equal(2))
+			})
+		})
+	})
+
 })
