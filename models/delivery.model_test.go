c5ff99b58e0cab07223b8f62773b8cff3ed7a355
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-06-06 22:29:51 +0200

    Added delivery method tests

--- models/delivery.model_test.go
+++ models/delivery.model_test.go
@@ -4,29 +4,83 @@ import (
 	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"gorm.io/gorm"
 )
 
-var _ = Describe("Devliery model", func() {
-	var deliveryMethod models.DeliveryMethod
-	var delivery models.Delivery
+var _ = Describe("Delivery model", func() {
+	var (
+		deliveryMethod models.DeliveryMethod
+		delivery       models.Delivery
+		paymentMethod  models.PaymentMethod
+		payment        models.Payment
+		order          models.Order
+	)
+
 	BeforeEach(func() {
 		deliveryMethod = CreateDeliveryMethod()
 		delivery = CreateDelivery(deliveryMethod)
 	})
 
-	AfterEach(func() {
-		delivery.Delete()
-		deliveryMethod.Delete()
+	Describe("Basic crud operations", func() {
+		Context("Update or delete operations", func() {
+			AfterEach(func() {
+				delivery.Delete()
+				deliveryMethod.Delete()
+			})
+			It("Should create new delivery", func() {
+				obtainedDelivery := models.Delivery{}
+				err := obtainedDelivery.SetID(delivery.GetID())
+				Expect(err).ShouldNot(HaveOccurred())
+				err = obtainedDelivery.FetchById()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(obtainedDelivery.DestinationCity).To(Equal(delivery.DestinationCity))
+			})
+			It("Should update existing delivery", func() {
+				obtainedDelivery := models.Delivery{}
+				err := obtainedDelivery.SetID(delivery.GetID())
+				Expect(err).ShouldNot(HaveOccurred())
+				delivery.DestinationAddress = "Poziomkowa 20"
+				err = delivery.UpdateInstance()
+				Expect(err).ShouldNot(HaveOccurred())
+				err = obtainedDelivery.FetchById()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(obtainedDelivery.DestinationAddress).To(Equal(delivery.DestinationAddress))
+			})
+		})
+		Context("Delete operations", func() {
+			AfterEach(func() {
+				deliveryMethod.Delete()
+			})
+			It("Should delete delivery", func() {
+				obtainedDelivery := models.Delivery{}
+				err := obtainedDelivery.SetID(delivery.GetID())
+				Expect(err).ShouldNot(HaveOccurred())
+				err = delivery.Delete()
+				Expect(err).ShouldNot(HaveOccurred())
+				err = obtainedDelivery.FetchById()
+				Expect(err).To(Equal(gorm.ErrRecordNotFound))
+			})
+		})
+
 	})
 
-	Describe("Basic crud operations", func() {
-		It("Should be in db", func() {
-			obtainedDelivery := models.Delivery{}
-			err := obtainedDelivery.SetID(delivery.GetID())
-			Expect(err).To(BeNil())
-			err = obtainedDelivery.FetchById()
-			Expect(err).To(BeNil())
-			Expect(obtainedDelivery.DestinationCity).To(Equal(delivery.DestinationCity))
+	Describe("Relationship test", func() {
+		BeforeEach(func() {
+			paymentMethod = CreatePaymentMethod()
+			payment = CreatePayment(paymentMethod)
+			order = CreateOrder(payment, delivery)
+		})
+		AfterEach(func() {
+			order.Delete()
+		})
+		Context("One order", func() {
+			It("Should fetch exactly one order", func() {
+				err := delivery.GetOrders()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(len(delivery.Order)).To(Equal(1))
+				Expect(delivery.Order[0].Price).To(Equal(order.Price))
+			})
+
 		})
 	})
 
