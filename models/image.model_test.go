f4971d9e0aa0213779a9bed7f656fe6e3c038ac2
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-06-07 21:40:24 +0200

    Added tests for image model

--- models/image.model_test.go
+++ models/image.model_test.go
@@ -4,26 +4,126 @@ import (
 	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"gorm.io/gorm"
 )
 
 var _ = Describe("Image Model", func() {
-	var image models.Image
+	var (
+		image    models.Image
+		category models.Category
+		customer models.Customer
+		product  models.Product
+		product2 models.Product
+		image2   models.Image
+	)
 
 	BeforeEach(func() {
 		image = CreateImage()
 	})
 
-	AfterEach(func() {
-		image.Delete()
+	Describe("Basic CRUD test", func() {
+		var obtainedImage models.Image
+
+		BeforeEach(func() {
+			obtainedImage = models.Image{}
+		})
+		Context("Create or update method", func() {
+			AfterEach(func() {
+				image.Delete()
+			})
+			It("Should create a new image", func() {
+				obtainedImage.SetID(image.GetID())
+				err := obtainedImage.FetchById()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(obtainedImage.FullPath).To(Equal(image.FullPath))
+			})
+
+			It("Should update an existing image", func() {
+				err := obtainedImage.SetID(image.GetID())
+				Expect(err).ShouldNot(HaveOccurred())
+				image.FullPath = "/tmp/image_new"
+				err = image.UpdateInstance()
+				Expect(err).ShouldNot(HaveOccurred())
+				err = obtainedImage.FetchById()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(obtainedImage.FullPath).To(Equal(image.FullPath))
+			})
+		})
+		Context("Delete method", func() {
+			It("Should delete an image", func() {
+				err := obtainedImage.SetID(image.GetID())
+				Expect(err).ShouldNot(HaveOccurred())
+				err = image.Delete()
+				Expect(err).ShouldNot(HaveOccurred())
+				err = obtainedImage.FetchById()
+				Expect(err).To(Equal(gorm.ErrRecordNotFound))
+				Expect(obtainedImage.FullPath).To(Equal(""))
+			})
+
+		})
+
 	})
 
-	Describe("Basic CRUD test", func() {
-		It("Should be stored into db", func() {
-			var obtainedImage models.Image
-			obtainedImage.SetID(image.GetID())
-			err := obtainedImage.FetchById()
-			Expect(err).To(BeNil())
-			Expect(obtainedImage.FullPath).To(Equal(image.FullPath))
+	Describe("Relationship test", func() {
+		BeforeEach(func() {
+			category = CreateCategory()
+			customer = CreateCustomer()
+			product = CreateProductWithoutOrder(category, image, customer)
+		})
+
+		AfterEach(func() {
+			product.Delete()
+			product2.Delete()
+			customer.Delete()
+			category.Delete()
+			image.Delete()
+			image2.Delete()
+		})
+
+		Context("One product", func() {
+			It("Should fetch one product", func() {
+				err := image.GetProduct()
+				Expect(err).ShouldNot(HaveOccurred())
+
+			})
+		})
+	})
+
+	Describe("One to one relationship test", func() {
+		BeforeEach(func() {
+			product2 = models.Product{
+				UnitPrice:   14,
+				ProductName: "sample_image2",
+				Quantity:    3,
+			}
+			image2 = models.Image{
+				Name:     "other_img",
+				FullPath: "/tmp/other_img",
+				Size:     1200,
+			}
+		})
+
+		JustBeforeEach(func() {
+			category = CreateCategory()
+			customer = CreateCustomer()
+			product = CreateProductWithoutOrder(category, image, customer)
+			CreateCustomImage(&image2)
+		})
+		AfterEach(func() {
+			product2.Delete()
+			product.Delete()
+			customer.Delete()
+			category.Delete()
+			image.Delete()
+			image2.Delete()
+		})
+
+		Context("Two products to one image", func() {
+			It("Should fail", func() {
+				CreateCustomProductWithoutOrder(category, image2, customer, &product2)
+				err := image.AssignTo(&product2)
+				Expect(err).Should(HaveOccurred())
+			})
 		})
 	})
 })
