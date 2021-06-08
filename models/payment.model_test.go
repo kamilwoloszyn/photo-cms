b404e984bbe2006fffc5a72398ef981695b94710
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-06-08 22:03:59 +0200

    Added payment tests

--- models/payment.model_test.go
+++ models/payment.model_test.go
@@ -1,9 +1,11 @@
 package models_test
 
 import (
+	"github.com/google/uuid"
 	"github.com/kamilwoloszyn/photo-cms/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"gorm.io/gorm"
 )
 
 var _ = Describe("Payment model", func() {
@@ -21,13 +23,71 @@ var _ = Describe("Payment model", func() {
 		paymentMethod.Delete()
 	})
 	Describe("Basic crud testing", func() {
-		It("Should contain payment in db", func() {
-			var obtainedPayment models.Payment
-			err := obtainedPayment.SetID(payment.GetID())
-			Expect(err).To(BeNil())
-			err = obtainedPayment.FetchByID()
-			Expect(err).To(BeNil())
-			Expect(obtainedPayment.PaymentAmount).To(Equal(payment.PaymentAmount))
+		var (
+			obtainedPayment models.Payment
+		)
+
+		BeforeEach(func() {
+			obtainedPayment = models.Payment{}
+		})
+		Context("Add or update an operations", func() {
+			It("Should create a new payment in db", func() {
+				err := obtainedPayment.SetID(payment.GetID())
+				Expect(err).ShouldNot(HaveOccurred())
+				err = obtainedPayment.FetchByID()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(obtainedPayment.PaymentAmount).To(Equal(payment.PaymentAmount))
+			})
+			It("Should update an existing payment", func() {
+				err := obtainedPayment.SetID(payment.GetID())
+				Expect(err).ShouldNot(HaveOccurred())
+				payment.PaymentAmount = 100
+				err = payment.UpdateInstance()
+				Expect(err).ShouldNot(HaveOccurred())
+				err = obtainedPayment.FetchByID()
+				Expect(err).ShouldNot(HaveOccurred())
+				Expect(obtainedPayment.PaymentAmount).To(Equal(payment.PaymentAmount))
+			})
+		})
+		Context("Delete operations", func() {
+			It("Should delete existing payment", func() {
+				err := obtainedPayment.SetID(payment.GetID())
+				Expect(err).ShouldNot(HaveOccurred())
+				err = payment.Delete()
+				Expect(err).ShouldNot(HaveOccurred())
+				err = obtainedPayment.FetchByID()
+				Expect(err).To(Equal(gorm.ErrRecordNotFound))
+				Expect(obtainedPayment.PaymentMethodId).To(Equal(uuid.Nil))
+			})
+		})
+
+	})
+
+	Describe("Relationship test", func() {
+		var (
+			deliveryMethod models.DeliveryMethod
+			delivery       models.Delivery
+			order          models.Order
+			err            error
+		)
+
+		BeforeEach(func() {
+			deliveryMethod = CreateDeliveryMethod()
+			delivery = CreateDelivery(deliveryMethod)
+			order = CreateOrder(payment, delivery)
+		})
+		JustAfterEach(func() {
+			err = payment.AssignTo(&order)
+		})
+
+		AfterEach(func() {
+			order.Delete()
+			payment.Delete()
+			delivery.Delete()
+		})
+		Context("One order", func() {
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(payment.Order.Price).To(Equal(order.Price))
 		})
 	})
 })
