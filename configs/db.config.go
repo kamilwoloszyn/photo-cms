2b269ef76df4cb3a3f438e0073a528930277ac3d
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-06-10 20:31:13 +0200

    refactor: error string from small letters

--- configs/db.config.go
+++ configs/db.config.go
@@ -22,19 +22,19 @@ func LoadDbConfig() (*DbConfig, error) {
 		return nil, err
 	}
 	if _, found := os.LookupEnv("STORAGE_HANDLER"); !found {
-		return nil, errors.New("Variable STORAGE_HANDLER is not present")
+		return nil, errors.New("variable STORAGE_HANDLER is not present")
 	}
 	if _, found := os.LookupEnv("STORAGE_HOST"); !found {
-		return nil, errors.New("Variable STORAGE_HOST is not present")
+		return nil, errors.New("variable STORAGE_HOST is not present")
 	}
 	if _, found := os.LookupEnv("STORAGE_DBUSER"); !found {
-		return nil, errors.New("Variable STORAGE_DBUSER is not present")
+		return nil, errors.New("variable STORAGE_DBUSER is not present")
 	}
 	if _, found := os.LookupEnv("STORAGE_DBPASSWORD"); !found {
-		return nil, errors.New("Variable STORAGE_DBPASSWORD is not present")
+		return nil, errors.New("variable STORAGE_DBPASSWORD is not present")
 	}
 	if _, found := os.LookupEnv("STORAGE_DATABASE"); !found {
-		return nil, errors.New("Variable STORAGE_DATABASE is not present")
+		return nil, errors.New("variable STORAGE_DATABASE is not present")
 	}
 	var config = DbConfig{
 		HandlerName: os.Getenv("STORAGE_HANDLER"),
