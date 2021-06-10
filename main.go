6261d23793497a17159c9edc3c259a61e2377775
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-06-10 20:27:43 +0200

    Updated main

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/kamilwoloszyn/photo-cms/configs"
+	"github.com/kamilwoloszyn/photo-cms/http/routes"
+	"github.com/pkg/errors"
+)
 
 func main() {
-	fmt.Println("It works")
+	fmt.Println("[photo-cms]: Server starting ...")
+	server, err := configs.NewServer()
+	if err != nil {
+		errWrapped := errors.Wrap(err, "Creating a new server")
+		fmt.Printf("[photo-cms]: Cannot create a new server: %s \n", errWrapped.Error())
+	}
+	routes.ApplyRoutes(server)
+	err = server.Listen()
+	if err != nil {
+		errWrapped := errors.Wrap(err, "Cannot start a new server")
+		fmt.Printf("[photo-cms]: Cannot start a server: %s ", errWrapped.Error())
+	}
 }
